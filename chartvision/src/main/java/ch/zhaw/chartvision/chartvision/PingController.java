e6af71542828ecaa58e3c16dec15bf991f3ff035
Sandro Uhler <uhlersan@students.zhaw.ch>
2025-04-22 15:34:14 +0200

    update

---
+++ chartvision/src/main/java/ch/zhaw/chartvision/chartvision/PingController.java
@@ -0,0 +1,13 @@
+package ch.zhaw.chartvision.chartvision;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PingController {
+
+    @GetMapping("/ping")
+    public String ping() {
+        return "ChartVision app is up and running!";
+    }
+}
