8967339c614b56d808451d514d159a7e2bd4fa1d
Sandro Uhler <uhlersan@students.zhaw.ch>
2025-04-22 11:34:11 +0200

    Spring Boot

---
+++ chartvision/src/main/java/ch/zhaw/chartvision/chartvision/ChartvisionApplication.java
@@ -0,0 +1,13 @@
+package ch.zhaw.chartvision.chartvision;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChartvisionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChartvisionApplication.class, args);
+	}
+
+}
---
+++ chartvision/src/test/java/ch/zhaw/chartvision/chartvision/ChartvisionApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.zhaw.chartvision.chartvision;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChartvisionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
